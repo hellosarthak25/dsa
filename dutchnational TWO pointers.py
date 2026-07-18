ea01e32ea23d1db6cd861b3e477ba4baa5b342c8
Sarthak Mishra <hellosarthak25@gmail.com>
2026-07-18 16:12:56 +0530

    Add files via upload

---
+++ dutchnational TWO pointers.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        start=0
+        end=len(nums)-1
+        mid=0
+        while(mid<=end):
+            if nums[mid]==0:
+                nums[start],nums[mid]=nums[mid],nums[start]
+                start+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            elif nums[mid]==2:
+                nums[mid],nums[end]=nums[end],nums[mid]
+                
+                end-=1
+        return nums
