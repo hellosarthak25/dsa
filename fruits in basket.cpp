2edcf658b262f8464e7c82d953592811eb0951fd
Sarthak Mishra <hellosarthak25@gmail.com>
2026-07-23 16:14:02 +0530

    Add files via upload

---
+++ fruits in basket.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+
+        int low = 0;
+        int n = fruits.size();
+        int res = 0;
+
+        unordered_map<int,int> f;
+
+        for(int high = 0; high < n; high++) {
+
+            f[fruits[high]]++;
+
+            while(f.size() > 2) {
+
+                f[fruits[low]]--;
+
+                if(f[fruits[low]] == 0) {
+                    f.erase(fruits[low]);
+                }
+
+                low++;
+            }
+
+            res = max(res, high - low + 1);
+        }
+
+        return res;
+    }
+};
