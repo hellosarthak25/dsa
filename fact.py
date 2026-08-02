ee01c2770a5216edbd0dbec6cab1e91ac0728989
Sarthak Mishra <hellosarthak25@gmail.com>
2026-08-02 19:16:29 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,9 @@
+class Solution:
+    def factorial(self, n: int) -> int:
+        fact=1
+        i=1
+        while(i<n+1):
+            fact=fact*i
+            i+=1
+        return fact
+        
