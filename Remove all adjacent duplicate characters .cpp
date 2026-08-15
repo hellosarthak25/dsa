792f7e4de646cc0e9b7a1484b1be361629fd6d4e
Sarthak Mishra <hellosarthak25@gmail.com>
2026-08-16 02:51:22 +0530

    Add files via upload

---
+++ Remove all adjacent duplicate characters .cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        int i;
+        string res;
+        int n = s.size();
+        for (i=0;i<n;i++){
+            if(st.empty()){
+                st.push(s[i]);
+                continue;
+            }
+            if(st.top()==s[i]){
+                st.pop();
+                continue;
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+            }
+        reverse(res.begin(),res.end());
+        return res;
+
+    }
+};
