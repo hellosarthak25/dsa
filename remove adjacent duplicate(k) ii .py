6a6648c308b82bd2c974e64657ae6ae8800c3984
Sarthak Mishra <hellosarthak25@gmail.com>
2026-08-16 23:27:19 +0530

    Add files via upload

---
+++ remove adjacent duplicate(k) ii .py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        st=[]
+        n=len(s)
+        
+        for ch in s:
+            if st and st[-1][0]==ch:
+                st[-1][1]+=1
+            else:
+                st.append([ch,1])
+            if st[-1][1]==k:
+                st.pop()
+        res=""
+        for ch,count in st:
+            res+=ch*count
+        return res
+
+
+                
+        
