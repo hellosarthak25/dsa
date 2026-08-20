bdf12e053313c526d4d6bd8aef50da61631db3d4
Sarthak Mishra <hellosarthak25@gmail.com>
2026-08-20 17:51:11 +0530

    Add files via upload

---
+++ swapnodes in pair .py
@@ -0,0 +1,60 @@
+class Solution:
+
+    def reverse(self, head: Optional[ListNode], times: int):
+        curr = head
+        prev = None
+
+        while times:
+            nex = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nex
+            times -= 1
+
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if head == None:
+            return None
+
+        left = head
+        right = None
+        res = None
+        prevleft = None
+        size = 2
+
+        while True:
+            right = left
+
+            for i in range(size - 1):
+                if right == None:
+                    break
+
+                right = right.next
+
+            if right:
+
+                nextleft = right.next
+
+                self.reverse(left, size)
+
+                if prevleft:
+                    prevleft.next = right
+
+                prevleft = left
+
+                if res == None:
+                    res = right
+
+                left = nextleft
+
+            else:
+
+                if prevleft:
+                    prevleft.next = left
+
+                if res == None:
+                    res = left
+
+                break
+
+        return res
