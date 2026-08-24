edac52d63e32cc433ca88b2d125166799c9e930c
Sarthak Mishra <hellosarthak25@gmail.com>
2026-08-25 03:18:00 +0530

    Add files via upload

---
+++ largest odd number in string .py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for i in range(len(num)-1,-1,-1):
+            if num[i] in "13579":
+                return num[0:i+1]
+        return ""
+
+        
